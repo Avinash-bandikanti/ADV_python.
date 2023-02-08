5acedd8ff741eb1bf568e34dc5f384e5b82778a1
user798 <avinashbandikanti1289@gmail.com>
2023-02-08 10:09:13 +0530

    advancepy

---
+++ numpy/demo_random.py
@@ -0,0 +1,7 @@
+import numpy as np
+#print(np.random.rand(5))
+#print(np.random.rand(4,5))
+#print(np.random.rand(2,2,2))
+#print(np.random.randn(5))
+print(np.random.randint(1,5,4))
+print(np.random.normal(0,1,4))
